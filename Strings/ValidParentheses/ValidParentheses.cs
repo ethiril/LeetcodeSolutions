04e4828a830773a8cf3228d17e6a249ff2a23483
Michael Stachowicz <michal.stachowicz@ibm.com>
2020-09-30 13:29:31 +0100

    strings exercises

---
+++ Strings/ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Strings.ValidParentheses
+{
+    class ValidParentheses
+    {
+
+        public bool IsValid(string s)
+        {
+            Dictionary<char, char> mappings = new Dictionary<char, char>();
+            mappings.Add('(', ')');
+            mappings.Add('{', '}');
+            mappings.Add('[', ']');
+
+            Stack<char> stack = new Stack<char>();
+            char[] chars = s.ToCharArray();
+
+            foreach (char c in chars)
+            {
+                if (mappings.ContainsKey(c))
+                {
+                    stack.Push(mappings[c]);
+                }
+                else
+                {
+                    if (stack.Count == 0 || c != stack.Pop())
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
