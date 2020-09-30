21630a94a0c01383f95846b9596255e37089de90
Michael Stachowicz <michal.stachowicz@ibm.com>
2020-09-30 13:30:37 +0100

    stacks exercises

---
+++ Stacks/EvalRPN/EvaluateRPN.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeProblems.Stacks.EvalRPN
+{
+    class EvaluateRPN
+    {
+        public int evalRPN(string[] tokens)
+        {
+            int a, b;
+            Stack<int> S = new Stack<int>();
+            foreach (string s in tokens)
+            {
+                if (s.Equals("+"))
+                {
+                    S.Push(S.Pop() + S.Pop());
+                }
+                else if (s.Equals("/"))
+                {
+                    b = S.Pop();
+                    a = S.Pop();
+                    S.Push(a / b);
+                }
+                else if (s.Equals("*"))
+                {
+                    S.Push(S.Pop() * S.Pop());
+                }
+                else if (s.Equals("-"))
+                {
+                    b = S.Pop();
+                    a = S.Pop();
+                    S.Push(a - b);
+                }
+                else
+                {
+                    S.Push(Int32.Parse(s));
+                }
+            }
+            return S.Pop();
+
+        }
+    }
+}
