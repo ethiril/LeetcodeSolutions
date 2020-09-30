59fd3e540b04d3bff6d28d27ce0a938af6841e47
Michael Stachowicz <michal.stachowicz@ibm.com>
2020-09-30 13:33:33 +0100

    Hashtables exercises

---
+++ HashTables/ValidAnagram/ValidAnagram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace LeetCodeProblems.HashTables.ValidAnagram
+{
+    class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+            int[] counter = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                counter[s[i] - 'a']++;
+                counter[t[i] - 'a']--;
+            }
+            foreach (int count in counter)
+            {
+                if (count != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
